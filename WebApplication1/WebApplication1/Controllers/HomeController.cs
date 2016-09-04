ea1ec3070845345258accebfabc06691e23b42c1
pablo gennoni <pablo gennoni>
2016-09-04 19:16:50 -0300

    initial commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var persona = new Persona();
+            return View(persona);
+        }
+        [HttpPost]
+        public ActionResult Index(Persona persona)
+        {
+            return View(persona);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Persona
+    {
+        public string Saludo { get; set; }
+        public string Nombre { get; set; }
+        /*public Persona()
+        {
+            this.Nombre = string.Empty;
+            this.Saludo = "Hola ";
+        }*/
+    }
+}
